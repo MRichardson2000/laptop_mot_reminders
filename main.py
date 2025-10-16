a2ee651236361211dc96ebdb23d31618f9793e49
Marcus <marcusrichardson7838@gmail.com>
2025-10-16 11:56:49 +0100

    put email back to helpdesk

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def send_reminder(reminder_list: list) -> None:
     outlook = win32com.client.Dispatch("Outlook.Application")
     mail = outlook.CreateItem(0)
     mail.Subject = "MOT Reminders"
-    mail.To = "marcus.richardson@ecology.co.uk"
+    mail.To = "helpdesk@ecology.co.uk"
     mail.Body = "The following MOTs are due within the next 30 days:\n\n" + "\n".join(
         reminder_list
     )
